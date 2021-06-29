f655aa04ff301a3b859fae194d6c9e6a47071055
Sandra Wazaz <sandrawazaz@gmail.com>
2021-06-29 12:36:31 -0400

    commit tree 21f07aa045e420f5b34df5d6c28d70ce6cb9c897

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(customerName){
+customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob'
+}
